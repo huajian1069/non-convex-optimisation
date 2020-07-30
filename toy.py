62a89efab9a50157ff736a3be3454d428d8a8ca7
huajian1069 <huajian1069@gmail.com>
2020-07-30 22:15:07 +0200

    get code snippets ready for start testing

--- toy.py
+++ toy.py
@@ -1,4 +1,14 @@
+
+import os
+import sys
+import time
+import numpy as np
+import math
+import json
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 x = torch.rand(10)
 x = x.cuda()
 print(2*x)
